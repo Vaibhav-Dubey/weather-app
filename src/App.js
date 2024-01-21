38ad0af0ccba12335b73327a08592624976a0be9
Vaibhav Dubey <56965007+Vaibhav-Dubey@users.noreply.github.com>
2024-01-21 00:21:28 -0700

    added basic functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+const [lat, setLat] = useState([]);
+const [long, setLong] = useState([]);
+const [data, setData] = useState([]);
+useEffect(() => {
+  const fetchData = async () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+    .then(res => res.json())
+    .then(result => {
+      setData(result)
+      console.log(result,process.env.REACT_APP_API_URL);
+    });
+  }
+  fetchData();
+}, [lat,long])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
